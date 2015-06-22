34a4b471e276879d991ace2aa3451b9e7f7bcdf7
Christopher Carson <chris@nowzoo.com>
2015-06-22 18:44:36 -0500

    Commit before publishing on Monday, June 22, 2015 6:44 PM

--- grunt/publish.js
+++ grunt/publish.js
@@ -60,7 +60,8 @@ module.exports = function (grunt, done) {
                         console.log('New Commit', result);
                     })
                     .then(function(){
-                        repo.checkoutBranch('gh-pages');
+                        var result = repo.checkoutBranch('gh-pages');
+                        console.log(result)
                     })
                     .then(function(){
                         callback();
@@ -71,11 +72,11 @@ module.exports = function (grunt, done) {
                 var files;
                 fs.readdir(process.cwd(), function(err, result){
                     files = result;
-                    _.each(files, function(filename){
-                        if (_.indexOf(['_site', '.gitignore'], filename) !== -1) return;
-                        if (filename.indexOf('.') === 0) return;
-                        grunt.file.delete(path.join(process.cwd(), filename));
-                    });
+                    //_.each(files, function(filename){
+                    //    if (_.indexOf(['_site', '.gitignore'], filename) !== -1) return;
+                    //    if (filename.indexOf('.') === 0) return;
+                    //    grunt.file.delete(path.join(process.cwd(), filename));
+                    //});
                     callback();
                 });
 
