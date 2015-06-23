49240c0a90c8d101ca95ee38305441a8eccf130d
Christopher Carson <chris@nowzoo.com>
2015-06-23 11:24:53 -0500

    Commit before publishing on Tuesday, June 23, 2015 11:24 AM

--- grunt/publish.js
+++ grunt/publish.js
@@ -5,13 +5,13 @@ module.exports = function (grunt, done) {
     var path = require('path');
     var Git = require('nodegit');
     var moment = require('moment');
-    var async = require('async');
     var ncp = require('ncp').ncp;
     var rimraf = require('rimraf');
     var promisify = require('promisify-node');
     var _ = require('lodash');
 
     var fs = promisify('fs');
+    var async = promisify('async');
 
     var repo;
     var index;
@@ -72,13 +72,19 @@ module.exports = function (grunt, done) {
             return fs.readdir(process.cwd());
         })
         .then(function(files){
-            console.log(files)
+            var keep = [ '.git', '.gitignore', '.idea', '_site', 'node_modules' ];
+            grunt.log.write('Deleting content...');
+            _.each(files, function(filename){
+                if (_.indexOf(keep, filename) >= 0) return callback();
+                if (filename.indexOf('.') === 0) return callback();
+                rimraf.sync(filename);
+            });
         })
         .then(function() {
             return repo.getRemote('origin');
         })
         .then(function(){
-            grunt.log.subhead('Checking out master...');
+            grunt.log.subhead('Checking out master.');
             return repo.checkoutBranch('master');
         })
 
@@ -90,79 +96,8 @@ module.exports = function (grunt, done) {
         .done(function(){
             done();
         });
-    //
-    //
-    //Git.Repository
-    //    .open(path.resolve(process.cwd(), './.git'))
-    //    .then(function(result) {
-    //        repo = result;
-    //    })
-    //    .then(function(){
-    //        grunt.log.subhead('Checking out master...');
-    //        return repo.checkoutBranch('master');
-    //    })
-    //    .then(function(){
-    //        console.log('got here')
-    //        grunt.log.subhead('On branch master:');
-    //        grunt.log('Committing changes...');
-    //        return repo.openIndex()
-    //    })
-    //    .then(function(result) {
-    //        console.log('got here')
-    //        index = result;
-    //        return index.read(1);
-    //    })
-    //    .then(function() {
-    //        return index.addAll();
-    //    })
-    //    .then(function() {
-    //        return index.write();
-    //    })
-    //    .then(function() {
-    //        return index.writeTree();
-    //    })
-    //    .then(function(result) {
-    //
-    //        oid = result;
-    //        return Git.Reference.nameToId(repo, "HEAD");
-    //    })
-    //    .then(function(result) {
-    //        return repo.getCommit(result);
-    //    })
-    //    .then(function(result) {
-    //        var signature = Git.Signature.default(repo);
-    //        var msg = 'Nog commit on master before publishing on ' + moment().format('LLLL');
-    //        return repo.createCommit("HEAD", signature, signature, msg, oid, [result]);
-    //    })
-    //    .then(function(result) {
-    //        grunt.log('Changes committed on branch master.', result);
-    //    })
 
 
-
-    //async.series(
-    //    [
-    //        function(callback){
-    //
-    //
-    //        },
-    //        function(callback){
-    //
-    //            fs.readdir(process.cwd(), function(err, result){
-    //                files = result;
-    //                callback();
-    //            });
-    //
-    //        },
-    //        function(callback){
-    //            var keep = [ '.git', '.gitignore', '.idea', '_site', 'node_modules' ];
-    //            async.each(files, function(filename, callback){
-    //                if (_.indexOf(keep, filename) >= 0) return callback();
-    //                if (filename.indexOf('.') === 0) return callback();
-    //                rimraf(filename, callback);
-    //            }, callback)
-    //
-    //        },
     //        function(callback){
     //            var p = path.join(process.cwd(), '_site');
     //            fs.readdir(p, function(err, result){
