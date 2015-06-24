f5d8102389cb2a7fa51831371c32d809e5a514ac
Christopher Carson <chris@nowzoo.com>
2015-06-23 22:45:44 -0500

    nog build on 2015-06-24T03:45:44.702Z

--- _nog/grunt/serve.js
+++ _nog/grunt/serve.js
@@ -4,13 +4,16 @@ module.exports = function (grunt, done) {
     var path = require('path');
     var express = require('express');
     var app = express();
-    var port = 3000;
-    app.use(express.static('_site'));
+
+    var site = grunt.config.get('nog');
+    var site_prefix = site.site_prefix || '';
+    var port = site.localhost_port || 3000;
+    app.use(site_prefix, express.static('./'));
     app.listen(port, function (err) {
         if (err) {
             grunt.log.err(err);
         } else {
-            grunt.log.oklns('Server listening on port %s. Go to  http://localhost:%s', port, port);
+            grunt.log.oklns('Server listening on port %s. Go to  http://localhost:%s%s', port, port, site_prefix);
             grunt.log.oklns('Press ^C to stop.')
 
         }
