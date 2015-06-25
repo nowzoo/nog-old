820b69652b9a57afaceb32a9e6a1101179008f84
Christopher Carson <chris@nowzoo.com>
2015-06-25 12:15:32 -0500

    be verbose

--- grunt/init.js
+++ grunt/init.js
@@ -16,7 +16,7 @@ module.exports = function(grunt, done){
             // Make sure we're on master
             function(callback){
                 var cmd = 'git checkout master';
-                grunt.verbose.writeln('Checking out master: %s', cmd);
+                grunt.log.writeln('Checking out master: %s', cmd);
                 exec(cmd, callback);
             },
 
@@ -24,7 +24,7 @@ module.exports = function(grunt, done){
             // Create gh-pages branch
             function(callback){
                 var cmd = 'git status --porcelain';
-                grunt.verbose.writeln('Checking git status: %s', cmd);
+                grunt.log.writeln('Checking git status: %s', cmd);
                 exec(cmd, function(err, stdout, stderr){
                     if (err) return callback(err);
                     if (stdout.length > 0) err = new Error('You have uncommitted changes on the master branch. Commit these changes first.');
@@ -36,14 +36,14 @@ module.exports = function(grunt, done){
             // Create gh-pages branch
             function(callback){
                 var cmd = 'git checkout --orphan gh-pages';
-                grunt.verbose.writeln('Creating gh-pages branch: %s', cmd);
+                grunt.log.writeln('Creating gh-pages branch: %s', cmd);
                 exec(cmd, callback);
             },
 
             // Empty gh-pages branch...
             function(callback){
                 var cmd = 'git rm -rf .';
-                grunt.verbose.writeln('Emptying gh-pages branch: %s', cmd);
+                grunt.log.writeln('Emptying gh-pages branch: %s', cmd);
                 exec(cmd, callback);
             },
 
@@ -55,28 +55,35 @@ module.exports = function(grunt, done){
             function(callback){
                 var dst = path.join(process.cwd(), '.gitignore');
                 var ignore = ['node_modules', '_site'];
-                grunt.verbose.writeln('Creating .gitignore');
+                grunt.log.writeln('Creating .gitignore');
                 fs.writeFile(dst, ignore.join('\n'), callback);
             },
 
             // Add the files to git on gh-pages...
             function(callback){
                 var cmd = 'git add -A';
-                grunt.verbose.writeln('Add: %s', cmd);
+                grunt.log.writeln('Add: %s', cmd);
                 exec(cmd, callback);
             },
 
             // Commit on gh-pages...
             function(callback){
-                var cmd = 'git commit -m "Initial nog commit on gh-pages"';
-                grunt.verbose.writeln('Commit: %s', cmd);
+                var cmd = 'git commit -m \'Initial nog commit on gh-pages\'';
+                grunt.log.writeln('Commit: %s', cmd);
+                exec(cmd, callback);
+            },
+
+            // Pull origin gh-pages...
+            function(callback){
+                var cmd = 'git pull origin gh-pages';
+                grunt.log.writeln('Pull: %s', cmd);
                 exec(cmd, callback);
             },
 
             // Checkout master
             function(callback){
                 var cmd = 'git checkout master';
-                grunt.verbose.writeln('Checking out master: %s', cmd);
+                grunt.log.writeln('Checking out master: %s', cmd);
                 exec(cmd, callback);
             }
 
