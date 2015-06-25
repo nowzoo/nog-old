996f6775cdd53dc2128c16095faea3920b01a474
Christopher Carson <chris@nowzoo.com>
2015-06-25 11:36:13 -0500

    fixed issue where master was getting overwritten

---
+++ grunt/init.js
@@ -0,0 +1,77 @@
+module.exports = function(grunt, done){
+    var async = require('async');
+    var fs =  require('fs');
+    var ncp =  require('ncp').ncp;
+    var path =  require('path');
+    var sprintf = require('sprintf-js').sprintf;
+    var exec = require('child_process').exec;
+
+
+    var lib_path = path.dirname(__dirname);
+
+
+    async.series(
+        [
+
+
+
+            // Create gh-pages branch
+            function(callback){
+                var cmd = 'git checkout --orphan gh-pages';
+                grunt.log.writeln('Creating gh-pages branch: %s', cmd);
+                exec(cmd, callback);
+            },
+
+            // Empty gh-pages branch...
+            function(callback){
+                var cmd = 'git rm -rf .';
+                grunt.log.writeln('Emptying gh-pages branch: %s', cmd);
+                exec(cmd, callback);
+            },
+
+            // Add the initial files that go in the gh-pages branch...
+            // - .gitignore created from scratch
+
+
+            // create .gitignore ...
+            function(callback){
+                var dst = path.join(process.cwd(), '.gitignore');
+                var ignore = ['node_modules', '_site'];
+                grunt.log.writeln('Creating .gitignore');
+                fs.writeFile(dst, ignore.join('\n'), callback);
+            },
+
+            // Add the files to git on gh-pages...
+            function(callback){
+                var cmd = 'git add -A';
+                grunt.log.writeln('Add: %s', cmd);
+                exec(cmd, callback);
+            },
+
+            // Commit on gh-pages...
+            function(callback){
+                var cmd = 'git commit -m "Initial nog commit on gh-pages"';
+                grunt.log.writeln('Commit: %s', cmd);
+                exec(cmd, callback);
+            },
+
+            // Checkout master
+            function(callback){
+                var cmd = 'git checkout master';
+                grunt.log.writeln('Checking out master: %s', cmd);
+                exec(cmd, callback);
+            }
+
+
+
+        ],
+        function(err){
+            if (err){
+                grunt.log.error(err);
+            } else {
+                grunt.log.ok('Site successfully initialized!');
+            }
+            done();
+        }
+    )
+}
