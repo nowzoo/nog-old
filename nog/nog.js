a8a5d56ee62a4ee66081fa712a2f19e5aa1d38b5
Christopher Carson <chris@nowzoo.com>
2015-06-29 16:17:14 -0500

    simplified

--- nog/nog.js
+++ nog/nog.js
@@ -8,12 +8,16 @@ module.exports.start = function (grunt, callback) {
     var temp = require('temp').track();
     var child_process = require('child_process');
     var gaze = require('gaze');
+    var express = require('express');
+    var tinylr = require('tiny-lr');
+
+
 
     var get_data = require('./get_data');
     var build = require('./build');
 
 
-    var new_data = {};
+    var site_serve_directory;
     var process_data;
     var temp_dir;
     var server;
@@ -24,8 +28,6 @@ module.exports.start = function (grunt, callback) {
     var data;
 
     process.on('SIGINT', function() {
-        reload_server.kill();
-        server.kill();
 	    callback();
 	});
 
@@ -37,62 +39,48 @@ module.exports.start = function (grunt, callback) {
                     if (err) return callback(err);
                     temp_dir = result;
                     grunt.log.writeln(temp_dir);
-                    new_data.directory = result.toString();
+                    site_serve_directory = result.toString();
                     callback(err)
                 });
             },
 
             // start the web server...
             function (callback) {
-                var p = path.join(process.cwd(), 'nog', 'serve.js');
                 var verbose = grunt.option('verbose') ? 'true' : 'false';
                 var port = grunt.option('port') || 3000;
                 var site_prefix = grunt.config('nog.site_prefix');
                 var started = false;
-                if (site_prefix.length == 0) site_prefix = '/';
+                var app = express();
                 grunt.log.write('Starting an express server... ');
-                server = child_process.spawn(
-                    p,
-                    [site_prefix, port, verbose],
-                    {stdio: ['ipc'], cwd: new_data.directory}
-                );
-                server.on('message', function(m) {
-                    if('started' === m.message){
-                        server_info = m;
-                        if (! m.err) grunt.log.ok();
-                        callback(m.err);
-                    }
+                if (site_prefix.length == 0) site_prefix = '/';
 
+                app.use(site_prefix, express.static(site_serve_directory));
+
+                app.listen(port, function (err) {
+                    server_info = {
+                        port: port,
+                        url: 'http://localhost:' + port + site_prefix,
+                        err: err || null
+                    };
+                    if (! err) grunt.log.ok();
+                    callback(err);
                 });
-
             },
 
             // Start the liverelod server...
             function (callback) {
-                var p = path.join(process.cwd(), 'nog', 'livereload.js');
-                var verbose = grunt.option('verbose') ? 'true' : 'false';
-                var port = grunt.option('port') || 3000;
-                var site_prefix = grunt.config('nog.site_prefix');
-                var started = false;
-                if (site_prefix.length == 0) site_prefix = '/';
-                grunt.log.write('Starting a livereload server... ');
-                reload_server = child_process.spawn(
-                    p,
-                    [site_prefix, port, verbose],
-                    {stdio: ['ipc'], cwd: new_data.directory}
-                );
-                reload_server.on('message', function(m) {
-                    if('started' === m.message){
-                        reload_server_info = m;
-                        if (! m.err) grunt.log.ok();
-                        return callback(m.err);
-                    }
-                    if('site_file_changed' === m.message){
-                        if (! m.err) grunt.log.ok(m.filepath, 'changed');
-                        else grunt.log.error(error);
-                        return callback(m.err);
-                    }
+                //standard LiveReload port
+                var livereload_port = 35729;
 
+                tinylr().listen(livereload_port, function(err) {
+                    var info;
+                    reload_server_info = {
+                        port: livereload_port,
+                        url: 'http://localhost:' + livereload_port,
+                        err: err || null
+                    };
+                    if (! err) grunt.log.ok();
+                    callback(err);
                 });
 
             },
@@ -100,7 +88,7 @@ module.exports.start = function (grunt, callback) {
             //build the site...
             function (callback) {
                 grunt.log.write('Building the site... ');
-                build(grunt, new_data.directory, function (err) {
+                build(grunt, site_serve_directory, function (err) {
                     if (! err) grunt.log.ok();
                     callback(err);
                 });
@@ -110,7 +98,7 @@ module.exports.start = function (grunt, callback) {
                 gaze(['_assets/**/*', '_content/**/*', '_templates/**/*'], function(err, watcher) {
                     this.on('all', function(event, filepath) {
                         grunt.log.write('Building the site... ');
-                        build(grunt, new_data.directory, function (err) {
+                        build(grunt, site_serve_directory, function (err) {
                             if (! err) grunt.log.ok();
                             else grunt.log.error(err);
                         });
